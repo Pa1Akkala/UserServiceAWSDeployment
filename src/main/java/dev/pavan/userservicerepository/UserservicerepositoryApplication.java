c1541539137a8f23e3c2e2ef9bc2f3c059899265
Pa1Akkala <pavanakkala.it@gmail.com>
2023-11-13 22:43:41 +0530

    first commit

---
+++ src/main/java/dev/pavan/userservicerepository/UserservicerepositoryApplication.java
@@ -0,0 +1,13 @@
+package dev.pavan.userservicerepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserservicerepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserservicerepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/pavan/userservicerepository/UserservicerepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pavan.userservicerepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserservicerepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
